015452549ae16407e2215e232df36f034d9b0834
Athimont <athimont1@hotmail.fr>
2016-03-29 10:24:29 +0200

    ajout des classes de test

---
+++ src/test/MainServerImplTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.*;
+import main.MainServeur;
+
+import org.junit.Test;
+
+public class MainServerImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
