ccf6a990c1bec12422224c6053ec171b0614f3b1
JuliennC <julienchaudoin@hotmail.fr>
2016-05-02 17:20:03 +0200

    Fonction qui détecte les changements d'une partie et qui appelle les fonction de l'interface en conséquence.

---
+++ src/Classes/Static.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Static {
+
+	
+	public final static Integer modificationJoueurs = 1;
+
+	public final static Integer modificationEtatPartie = 2;
+
+	public final static Integer modificationPartieCommence = 3;
+
+}
+
