c389a455431beaa86e79a666e1aeb228dea9d8cc
arkanyx <rodolphe.mathieu.blaizat@gmail.com>
2016-05-05 22:30:30 +0200

    Revert "Modification affichageTuilePioche"
    This reverts commit edae3c3b7d4cab011922800be808b759064da17c.

--- src/Classes/Plateau/Plateau.java
+++ src/Classes/Plateau/Plateau.java
@@ -27,7 +27,7 @@ public class Plateau implements Serializable {
 
 	/**
 	 * Initialisation du Plateau
-	 * C'est un tableau de Cases
+	 * C'est un tableau de Case
 	 * Plateau de 8 cases de largeur et 6 cases de hauteur
 	 * ATTENTION : dans un tableau à double dimension, c'est tab[y][x] !
 	 * 
